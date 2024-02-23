e89a798cc1cb23cc092c3489a9a4ac87f99af23e
PIERLUIGI AMATO <giga487@hotmail.com>
2024-02-23 18:40:06 +0100

    start with the communication protocol, inserimento client e server, ma gestione di account e creazione oggetti da integrare.

---
+++ ClientServer/Backend/Services/CharStatusService.cs
@@ -0,0 +1,13 @@
+﻿using CharStatus;
+using Grpc.Core;
+
+namespace Backend.Services
+{
+    public class CharStatusService : CharStatus.CharStatus.CharStatusBase
+    {
+        public override Task<MoveAck> Move(CharStatus.Status request, ServerCallContext context)
+        {
+            return base.Move(request, context);
+        }
+    }
+}
